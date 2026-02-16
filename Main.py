11bbd64fb3ca33de505c5cb3a765cec82121e5e2
simonethan648-prog <simonethan648@gmail.com>
2026-02-16 10:50:26 +0400

    Add files via upload
    Set the screen for turtle graphics, including its size and title. Then, set the screen color as per your choice

---
+++ Main.py
@@ -0,0 +1,5 @@
+import turtle
+turtle.Screen().bgcolor("Orange")
+sc = turtle.Screen()
+sc.setup(400, 300)
+turtle.title("Welcome to Turtle Window")
---
+++ Main2.py
@@ -0,0 +1,24 @@
+import turtle
+turtle.Screen().bgcolor("Green")
+board = turtle.Turtle()
+ 
+board.forward(100)
+
+board.left(120)
+board.forward(100)
+
+board.left(120)
+board.forward(100)
+
+
+board.forward(100)
+board.left(90)
+
+board.forward(100)
+board.left(90)
+
+board.forward(100)
+board.left(90)
+
+board.forward(100)
+board.left(90)
